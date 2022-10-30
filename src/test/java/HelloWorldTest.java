3e08f57bb5641a6edb0d822eae8224f0f1395109
Alex <kerleau@gmail.com>
2022-10-30 10:41:16 +0100

    Adding wrapper, auto clean ,  jar & siteplugin

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,9 @@
+public class HelloWorldTest {
+    public void testHello(){
+        System.out.println(" TEST : HelloWorldTest.testHello().........1");
+    }
+
+    public void testWorld(){
+        System.out.println("TEST : World ..... 1");
+    }
+}
