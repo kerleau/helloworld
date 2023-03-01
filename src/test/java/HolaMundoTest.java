bd35fc12ffca4905ff929cc4ecb480cf24686c98
kerleau <37364328+kerleau@users.noreply.github.com>
2023-03-01 19:05:48 +0000

    Adding a new Test java class

---
+++ src/test/java/HolaMundoTest.java
@@ -0,0 +1,16 @@
+/* test class
+ * 
+*/
+public class HolaMundoTest {
+
+    public void testHello(){
+
+        System.out.println("TEST - Holas...");
+    }
+
+    public void testWorld() {
+        System.out.println("TEST -MUNDO  World!!!!");
+    }
+
+    
+}
